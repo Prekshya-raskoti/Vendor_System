20cef47a4c6110c0c8a96522608fbe4d20060228
Prekshya <bisuraskoti@gmail.com>
2025-04-01 15:36:16 +0545

    vendor_portal setup

--- vendor_portal/asgi.py
+++ vendor_portal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vendor_portal_project project.
+ASGI config for vendor_portal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendor_portal_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendor_portal.settings')
 
 application = get_asgi_application()
--- vendor_portal/wsgi.py
+++ vendor_portal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vendor_portal_project project.
+WSGI config for vendor_portal.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendor_portal_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendor_portal.settings')
 
 application = get_wsgi_application()
